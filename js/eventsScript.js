d0339d85fa408f69a88699b5f8fecb281c92ce57
Gary <shengary1999@gmail.com>
2021-11-02 01:39:46 +1300

    Completed. Added restart + colours and text

--- js/eventsScript.js
+++ js/eventsScript.js
@@ -1,5 +1,6 @@
 const scoreTracker = document.querySelector('#scoreTracker');
 let gameRounds = 0;
+const playAgainButton = document.createElement('BUTTON');
 
 const div = document.createElement('div');
 div.style.backgroundColor='yellow';
@@ -10,12 +11,19 @@ div.style.display='flex';
 div.style.alignSelf='center';
 div.style.alignItems='center';
 div.style.justifyContent='center';
+div.style.flexDirection='column';
 div.style.whiteSpace='pre-wrap';
 
-div.textContent=`The current round is ${gameRounds}`
+div.textContent=`Make a choice to begin`
 //div.textContent = userPlay();
 
 scoreTracker.appendChild(div);
+
+//let gameEnd = false;
+
+let playerScore = 0;
+let computerScore = 0;
+const playerClickChoice = document.querySelectorAll('.bButton');
 
 function playerChoice(){
     if (this.classList.contains('paperButton')){
@@ -33,33 +41,81 @@ function playerChoice(){
     
 }
 
-const playerClickChoice = document.querySelectorAll('.bButton');
-playerClickChoice.forEach(playerClickChoice => playerClickChoice.addEventListener("click", playerChoice));
+function update(){
+    if(result == "You lose"){
+        computerScore += 1
+        console.log("computer score: " + computerScore);
+        div.style.backgroundColor='red';
+    }
+    else if(result == "You win"){
+        playerScore += 1
+        console.log("player score: " + playerScore);
+        div.style.backgroundColor='green';
+    }
+    else if (result == "Draw"){
+        div.style.backgroundColor='yellow';
+    }
+    else{
+    }
+    div.textContent = `The current round is ${gameRounds}`
+    div.textContent +=`\nYour current score is: ${playerScore}`
+    div.textContent +=`\nComputer current score is: ${computerScore}`
+    div.textContent +=`\nYou played: ${getUserChoiceCompare()}`
+    div.textContent +=`\nThe computer played: ${getComputerChoiceCompare()}`;
+    div.textContent +=`\n${result}`;
 
+}
+
+
+
+function gameEnd(){
+    playAgainButton.innerHTML = "Click me to restart";
+    playAgainButton.classList.add("restart");
+    playAgainButton.addEventListener('click', gameRestart)
+    div.appendChild(playAgainButton);
+}
+
+function gameRestart(){
+    if (playAgainButton.classList.contains("restart")){
+        playAgainButton.remove();
+    }
+    playerClickChoice.forEach(playerClickChoice => playerClickChoice.addEventListener("click", playerChoice));
+    div.style.backgroundColor='yellow';
+    playerScore = 0;
+    computerScore = 0;
+    gameRounds = 0;
+    div.textContent = `The current round is ${gameRounds}`
+}
 
 function gameTracker(choice){
-    while (gameRounds < 6){
+    while (gameRounds < 20){
         result = game(choice);
         console.log(gameRounds);
         update(result);
         break;
     }
 
+    if (playerScore == 5 || computerScore == 5){
+        playerClickChoice.forEach(playerClickChoice => playerClickChoice.removeEventListener("click", playerChoice));
+        
+        if(playerScore == 5){
+            div.textContent =`WINNER!!!`
+            div.textContent += `\nYou had ${playerScore} points.`
+            div.textContent += `\nYou won ${playerScore} points in ${gameRounds} rounds`
+        }
+        else{
+            div.textContent=`LOSER!!!`
+            div.textContent += `\nYou had ${playerScore} points.`
+            div.textContent += `\nComputer won ${computerScore} points in ${gameRounds} rounds`
+        }
+
+        //div.textContent.remove();
+        //console.log("test");
+        gameEnd();
+    }
+
 }
 
-function update(){
-    div.textContent = `The current round is ${gameRounds}\nYou played: ${getUserChoiceCompare()}\nThe computer played: ${getComputerChoiceCompare()}`;
-    div.textContent += `\n${result}`;
-
-    if(result == "You lose"){
-        div.style.backgroundColor='red';
-    }
-    else if(result == "You win"){
-        div.style.backgroundColor='green';
-    }
-    else{
-        div.style.backgroundColor='yellow';
-    }
-}
+gameRestart();
 
 //gameTracker();
